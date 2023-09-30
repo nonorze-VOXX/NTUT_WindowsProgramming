f8cb64e18ae6cae7da935180c3df0badc27b97ca
green-Desktop <appieosugood@gmail.com>
2023-09-30 22:02:01 +0800

    init powerpoint

--- Unity/Form1.Designer.cs
+++ Unity/Form1.Designer.cs
@@ -8,15 +8,6 @@ namespace Unity
 {
     partial class Form1
     {
-        private CalculatorModel _calculatorModel = new CalculatorModel();
-        /// <summary>
-        /// getter
-        /// </summary>
-        /// <returns></returns>
-        public CalculatorModel GetCalculatorModel()
-        {
-            return _calculatorModel;
-        }
         /// <summary>
         /// Required designer variable.
         /// </summary>
@@ -43,18 +34,7 @@ namespace Unity
         /// </summary>
         private void InitializeComponent()
         {
-            _buttons = new List<Button>();
-            this._textBox1 = new System.Windows.Forms.RichTextBox();
             this.SuspendLayout();
-            // richTextBox1
-            // 
-            this._textBox1.Location = new System.Drawing.Point(33, 28);
-            this._textBox1.Name = "textBox1";
-            this._textBox1.ReadOnly = true;
-            this._textBox1.Size = new System.Drawing.Size(240, 40);
-            this._textBox1.TabIndex = 1;
-            this._textBox1.Text = "";
-            this._textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 19F);
 
             // 
             // Form1
@@ -62,71 +42,13 @@ namespace Unity
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(300, 450);
-            this.Controls.Add(this._textBox1);
             
             this.Name = "Form1";
             this.Text = "Caculator";
             this.ResumeLayout(false);
             this.PerformLayout();
-
         }
-
-        /// <summary>
-        /// create
-        /// </summary>
-        private void CreateButtons()
-        {
-            List<List<string>> lists = new List<List<string>>();
-            lists.Add(new List<string> { "0", ".", "=", "/", });
-            lists.Add(new List<string> { "1", "2", "3", "*", } );
-            lists.Add(new List<string> { "4", "5", "6", "-", } );
-            lists.Add(new List<string> { "7", "8", "9", "+", } );
-            lists.Add(new List<string> { "", "", "CE", "C", });
-            for (var y = 0; y < lists.Count; y++)
-            {
-                var list = lists[y];
-                for (var x = 0; x < list.Count; x++)
-                {
-                    var o = list[x];
-                    if (o.Equals(""))
-                    {
-                        continue;
-                    }
-
-                    var button = new Button();
-                    button.Location = new System.Drawing.Point(25 + x * 60, 250 + 35 * 3 - y * 70);
-                    button.Name = "button" + o;
-                    button.Size = new System.Drawing.Size(60, 70);
-                    button.TabIndex = 0;
-                    button.Text = o;
-                    button.UseVisualStyleBackColor = true;
-                    button.Click += new System.EventHandler(
-                        (object sender, EventArgs e) =>
-                        {
-                            _calculatorModel.Input(o);
-                            GetTextBox1().Text = GetCalculatorModel().GetOutput();
-                            // GetTextBox1().Text = GetCalculatorModel()._result + " "
-                            //                                                   + GetCalculatorModel()._processingNumber +
-                            //                                                   " "
-                            //                                                   + GetCalculatorModel()._operator;
-                        });
-                    this.Controls.Add(button);
-                }
-            }
-        }
-        
         #endregion
-
-        /// <summary>
-        /// getTextBox1
-        /// </summary>
-        /// <returns></returns>
-        public System.Windows.Forms.RichTextBox GetTextBox1()
-        {
-            return _textBox1;
-        }
-        private List<Button> _buttons;
-        private System.Windows.Forms.RichTextBox _textBox1;
     }
 }
 
--- Unity/Form1.cs
+++ Unity/Form1.cs
@@ -7,16 +7,7 @@ namespace Unity
     {
         public Form1()
         {
-            _calculatorModel = new CalculatorModel();
             InitializeComponent();
-            CreateButtons();
-        }
-
-        public Form1(CalculatorModel calculatorModel)
-        {
-            _calculatorModel = calculatorModel;
-            InitializeComponent();
-            CreateButtons();
         }
     }
 }
