8aaa0591dd29d5c5bd82e9e717f0d6fb4af3118f
green-Desktop <appieosugood@gmail.com>
2023-11-28 13:24:40 +0800

    test form for coverage

---
+++ UnityTests/Form1Tests.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Unity.Tests
+{
+    [TestClass()]
+    public class Form1Tests
+    {
+        [TestMethod()]
+        public void Form1Test()
+        {
+            new Form1(new PresentationModel(new ShapeModel()));
+        }
+    }
+}
