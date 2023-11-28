582b71968fbbb90798b4dfd170e17882fb40b3cd
green-Desktop <appieosugood@gmail.com>
2023-11-28 21:23:15 +0800

    graphics test work

---
+++ UnityTests/WindowFormsGraphicsAdaptorTests.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Drawing;
+
+namespace Unity.Tests
+{
+    [TestClass()]
+    public class WindowFormsGraphicsAdaptorTests
+    {
+        private Graphics _graphics;
+        private WindowFormsGraphicsAdaptor windowFormsGraphicsAdaptor;
+
+
+        [TestInitialize()]
+        public void SetUp()
+        {
+            _graphics = Graphics.FromImage(new Bitmap(1, 1));
+            windowFormsGraphicsAdaptor = new WindowFormsGraphicsAdaptor(_graphics);
+        }
+
+        [TestMethod()]
+        public void DrawEllipseTest()
+        {
+            windowFormsGraphicsAdaptor.DrawEllipse(new Point2(0, 0), new Point2(0, 0));
+        }
+
+        [TestMethod()]
+        public void DrawEllipseByCenterAndSizeTest()
+        {
+            windowFormsGraphicsAdaptor.DrawEllipseByCenterAndSize(new Point2(1, 1), new Point2(1, 1));
+        }
+
+        [TestMethod()]
+        public void DrawLineTest()
+        {
+            windowFormsGraphicsAdaptor.DrawLine(new Point2(0, 0), new Point2(0, 0));
+        }
+
+        [TestMethod()]
+        public void DrawRectangleTest()
+        {
+            windowFormsGraphicsAdaptor.DrawRectangle(new Point2(0, 0), new Point2(0, 0));
+            windowFormsGraphicsAdaptor.DrawRectangle(new Point2(0, 0), new Point2(0, 0), Pens.Red);
+        }
+    }
+}
